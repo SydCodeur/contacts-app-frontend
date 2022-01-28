19d415df5cec98fbfddb551c311fcd5194a4213e
unknown <sydcodeur@gmail.com>
2022-01-28 20:18:33 +0000

    customize auth views

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,11 @@
 <template>
-    <div class="container">
-        <div class="login-margin-top"></div>
-        <div class="row">
-            <div class="col-lg-6">
-                <div v-if="mode == 'create'">
-                    <div
-                        class="alert alert-info"
-                        v-if="createAccountMessage"
-                    >{{ createAccountMessage }}</div>
-                </div>
-                <h1 v-if="mode == 'login'">Connexion</h1>
-                <h1 v-else>Inscription</h1>
-                <form>
+    <section class="container-fluid bg">
+        <section class="row justify-content-center">
+            <section class="col-12 col-sm-6 col-md-3">
+                <div class="form-container">
+                    <h3 class="text-center" v-if="mode == 'login'">Connexion</h3>
+                    <h3 class="text-center" v-else>Inscription</h3>
+
                     <small v-if="mode == 'login'" id="emailHelp" class="form-text text-muted">
                         Vous n'avez pas de compte ?
                         <a class="btn" @click="switchToCreateAccount">
@@ -24,139 +18,148 @@
                             <span style="color : #4fc08d;">Connectez-vous</span>
                         </a>
                     </small>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input
-                            v-model="email"
-                            type="email"
-                            class="form-control"
-                            id="email"
-                            aria-describedby="emailHelp"
-                            placeholder="Veuillez saisir votre email"
-                        />
-                    </div>
-                    <div v-if="mode == 'create'" class="form-group">
-                        <label for="firstname">Prénoms</label>
-                        <input
-                            v-model="firstName"
-                            type="text"
-                            class="form-control"
-                            id="firstname"
-                            placeholder="Veuillez saisir votre prénom"
-                        />
-                    </div>
-                    <div v-if="mode == 'create'" class="form-group">
-                        <label for="lastName">Nom</label>
-                        <input
-                            v-model="lastName"
-                            type="text"
-                            class="form-control"
-                            id="lastName"
-                            placeholder="Veuillez saisir votre nom"
-                        />
+
+                    <div v-if="mode == 'create'">
+                        <div
+                            class="alert alert-info"
+                            v-if="createAccountMessage"
+                        >{{ createAccountMessage }}</div>
                     </div>
 
-                    <div class="form-group">
-                        <label for="password">Mot de passe</label>
-                        <input
-                            v-model="password"
-                            type="password"
-                            class="form-control"
-                            id="password"
-                            placeholder="Veuillez saisir votre mot de passe"
-                        />
-                        <small
-                            v-if="mode == 'create'"
-                            class="form-text text-muted"
-                        >Le mot de passe doit comporter au moins 6 caractères</small>
+                    <form>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input
+                                v-model="email"
+                                type="email"
+                                class="form-control"
+                                id="email"
+                                aria-describedby="emailHelp"
+                                placeholder="Veuillez saisir votre email"
+                            />
+                        </div>
+                        <div v-if="mode == 'create'" class="form-group">
+                            <label for="firstname">Prénoms</label>
+                            <input
+                                v-model="firstName"
+                                type="text"
+                                class="form-control"
+                                id="firstname"
+                                placeholder="Veuillez saisir votre prénom"
+                            />
+                        </div>
+                        <div v-if="mode == 'create'" class="form-group">
+                            <label for="lastName">Nom</label>
+                            <input
+                                v-model="lastName"
+                                type="text"
+                                class="form-control"
+                                id="lastName"
+                                placeholder="Veuillez saisir votre nom"
+                            />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Mot de passe</label>
+                            <input
+                                v-model="password"
+                                type="password"
+                                class="form-control"
+                                id="password"
+                                placeholder="Veuillez saisir votre mot de passe"
+                            />
+                            <small
+                                v-if="mode == 'create'"
+                                class="form-text text-muted"
+                            >Le mot de passe doit comporter au moins 6 caractères</small>
+                        </div>
+                    </form>
+                    <div v-if="mode == 'create'">
+                        <div v-if="status == 'EMAIL_EXISTS'">
+                            <small class="form-text text-muted text-center">
+                                <span style="color: red;">Cet email est déjà utilisé</span>
+                            </small>
+                            <br />
+                        </div>
+                        <div v-else-if="status == 'INVALID_EMAIL'">
+                            <small class="form-text text-muted text-center">
+                                <span style="color: red;">Cet email est invalide</span>
+                            </small>
+                            <br />
+                        </div>
+                        <div v-else-if="status == 'PASSWORD_MIN_LENGTH_ERROR'">
+                            <small class="form-text text-muted text-center">
+                                <span
+                                    style="color: red;"
+                                >Le mot de passe doit comporter au moins 6 caractères</span>
+                            </small>
+                            <br />
+                        </div>
+                        <div v-else-if="status == 'FIRSTNAME_MIN_LENGTH_ERROR'">
+                            <small class="form-text text-muted text-center">
+                                <span
+                                    style="color: red;"
+                                >Le prénom doit comporter au moins 2 caractères</span>
+                            </small>
+                            <br />
+                        </div>
+                        <div v-else-if="status == 'LASTNAME_MIN_LENGTH_ERROR'">
+                            <small class="form-text text-muted text-center">
+                                <span
+                                    style="color: red;"
+                                >Le nom doit comporter au moins 2 caractères</span>
+                            </small>
+                            <br />
+                        </div>
+                        <div v-else-if="status == 'INTERNAL_ERROR'">
+                            <small class="form-text text-muted text-center">
+                                <span
+                                    style="color: red;"
+                                >Une erreur s'est produite. Veuillez réessayer plus tard</span>
+                            </small>
+                            <br />
+                        </div>
                     </div>
-                </form>
-                <!-- Error Management for create account -->
-                <div v-if="mode == 'create'">
-                    <div v-if="status == 'EMAIL_EXISTS'">
-                        <small class="form-text text-muted text-center">
-                            <span style="color: red;">Cet email est déjà utilisé</span>
-                        </small>
-                        <br />
+
+                    <div v-if="mode == 'login'">
+                        <div v-if="status == 'USER_NOT_FOUND'">
+                            <small class="form-text text-muted text-center">
+                                <span style="color: red;">Cet utilisateur est introuvable</span>
+                            </small>
+                            <br />
+                        </div>
+                        <div v-if="status == 'PASSWORD_INCORRECT'">
+                            <small class="form-text text-muted text-center">
+                                <span style="color: red;">Mot de passe incorrect</span>
+                            </small>
+                            <br />
+                        </div>
                     </div>
-                    <div v-else-if="status == 'INVALID_EMAIL'">
-                        <small class="form-text text-muted text-center">
-                            <span style="color: red;">Cet email est invalide</span>
-                        </small>
-                        <br />
-                    </div>
-                    <div v-else-if="status == 'PASSWORD_MIN_LENGTH_ERROR'">
-                        <small class="form-text text-muted text-center">
-                            <span
-                                style="color: red;"
-                            >Le mot de passe doit comporter au moins 6 caractères</span>
-                        </small>
-                        <br />
-                    </div>
-                    <div v-else-if="status == 'FIRSTNAME_MIN_LENGTH_ERROR'">
-                        <small class="form-text text-muted text-center">
-                            <span style="color: red;">Le prénom doit comporter au moins 2 caractères</span>
-                        </small>
-                        <br />
-                    </div>
-                    <div v-else-if="status == 'LASTNAME_MIN_LENGTH_ERROR'">
-                        <small class="form-text text-muted text-center">
-                            <span style="color: red;">Le nom doit comporter au moins 2 caractères</span>
-                        </small>
-                        <br />
-                    </div>
-                    <div v-else-if="status == 'INTERNAL_ERROR'">
-                        <small class="form-text text-muted text-center">
-                            <span
-                                style="color: red;"
-                            >Une erreur s'est produite. Veuillez réessayer plus tard</span>
-                        </small>
-                        <br />
-                    </div>
+                    <button
+                        v-if="mode == 'login'"
+                        @click="login"
+                        type="submit"
+                        class="btn btn-block auth-btn"
+                        :disabled="!validatedFields"
+                    >
+                        <span v-if="status == 'loading'">Chargement...</span>
+                        <span v-else>Se connecter</span>
+                    </button>
+
+                    <button
+                        @click="createAccount"
+                        v-else
+                        type="submit"
+                        class="btn btn-block auth-btn"
+                        :disabled="!validatedFields"
+                    >
+                        <span v-if="status == 'loading'">Chargement...</span>
+                        <span v-else>Créer un compte</span>
+                    </button>
                 </div>
-
-                <!-- End of Management -->
-
-                <div v-if="mode == 'login'">
-                    <div v-if="status == 'USER_NOT_FOUND'">
-                        <small class="form-text text-muted text-center">
-                            <span style="color: red;">Cet utilisateur est introuvable</span>
-                        </small>
-                        <br />
-                    </div>
-                    <div v-if="status == 'PASSWORD_INCORRECT'">
-                        <small class="form-text text-muted text-center">
-                            <span style="color: red;">Mot de passe incorrect</span>
-                        </small>
-                        <br />
-                    </div>
-                </div>
-
-                <span></span>
-                <button
-                    v-if="mode == 'login'"
-                    @click="login"
-                    type="submit"
-                    class="btn auth-btn"
-                    :disabled="!validatedFields"
-                >
-                    <span v-if="status == 'loading'">Chargement...</span>
-                    <span v-else>Se connecter</span>
-                </button>
-
-                <button
-                    @click="createAccount"
-                    v-else
-                    type="submit"
-                    class="btn auth-btn"
-                    :disabled="!validatedFields"
-                >
-                    <span v-if="status == 'loading'">Chargement...</span>
-                    <span v-else>Créer un compte</span>
-                </button>
-            </div>
-        </div>
-    </div>
+            </section>
+        </section>
+    </section>
 </template>
 
 <script>
@@ -245,9 +248,6 @@ export default {
 
 
 <style>
-.login-margin-top {
-    margin-top: 30px;
-}
 .auth-btn {
     background: #4fc08d;
     color: #fff;
@@ -255,5 +255,26 @@ export default {
 .auth-btn:hover {
     background-color: #4caf50;
     color: white;
+}
+
+.bg {
+    background: url("../assets/bg.jpg") no-repeat;
+    width: 100%;
+    height: 100vh;
+    background-size: 200%;
+}
+
+.form-container {
+    position: absolute;
+    top: 15vh;
+    background: #fff;
+    padding: 30px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 0px #000;
+}
+@media only screen and (max-width: 678px) {
+    .bg {
+        background-size: 300%;
+    }
 }
 </style>
